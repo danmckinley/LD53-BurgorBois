74914f0e2fce10d508d3e2603b705372f1729792
Dan McKinley <danmckinley54@gmail.com>
2023-04-30 17:19:35 +0100

    Began implementing gliding

--- Assets/Scripts/Bird/InputHandler.cs
+++ Assets/Scripts/Bird/InputHandler.cs
@@ -23,6 +23,7 @@ public class InputHandler : MonoBehaviour
     {
         playerInput = new PlayerInput();
         rb = GetComponent<Rigidbody2D>();
+        //rb.gravityScale = 0f;
     }
 
     private void Start()
@@ -42,17 +43,39 @@ public class InputHandler : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
-        rb.transform.rotation = Quaternion.Euler(0, 0,
-            Mathf.Atan2(mousePosition.y - rb.position.y, mousePosition.x - rb.position.x) * Mathf.Rad2Deg - 90);
+    {   
+        // todo : remove hard coded rotation towards mouse - apply it as a rotational force instead?
+        //rb.transform.rotation = Quaternion.Euler(0, 0,
+        //    Mathf.Atan2(mousePosition.y - rb.position.y, mousePosition.x - rb.position.x) * Mathf.Rad2Deg - 90);
 
-        Vector3 dir = (new Vector3(mousePosition.x - rb.transform.position.x, mousePosition.y - rb.transform.position.y,
-            rb.transform.position.z));
+        // torque - difference between current rotation and desired rotation
+        // Vector3 mouseDir = (new Vector3(mousePosition.x - rb.transform.position.x, mousePosition.y - rb.transform.position.y,
+        //    rb.transform.position.z));
+        
+        //Vector2 rb.transform.right.x - mousePosition.x
+        //float roll = Vector2.Angle((rb.transform.right), (mousePosition)); // figure out how to make torque
+        //transform.Rotate(transform.right, roll * Time.deltaTime * -10, Space.World);
 
+        //Vector2 mouseDir = new Vector2(mousePosition.x - rb.transform.position.x, mousePosition.y - rb.transform.position.y);
+        float mouseDistX = mousePosition.x - rb.transform.position.x;
+        float mouseDistY = mousePosition.y - rb.transform.position.y;
+        float angle = Mathf.Atan2(mouseDistX, mouseDistY) * Mathf.Rad2Deg; // desired rotation
 
-        Debug.Log($"Move speed {moveSpeed}");
+        //float currentRotation = rb.
+        transform.Rotate(transform.forward, angle * Time.deltaTime * -10, Space.Self);
 
-        rb.AddForce(dir.normalized * moveSpeed);
+        // gravity 
+        rb.velocity -= Vector2.up * Time.deltaTime;
+
+        // transform vertical velocity into horizontal velocity
+        Vector2 vertVel = rb.velocity - ((Vector2)(Vector3.Project(transform.up, rb.velocity)));
+        rb.velocity -= vertVel * Time.deltaTime;
+        rb.velocity = vertVel.magnitude * transform.right * Time.deltaTime / 10;
+
+        // drag
+        Vector2 drag = rb.velocity - (Vector2)(Vector3.Project(transform.right, rb.velocity));
+        rb.AddForce( -drag * drag.magnitude * Time.deltaTime / 1000);
+
     }
 
     private IEnumerator GetMoveSpeedRoutine(float prevSpeed)
