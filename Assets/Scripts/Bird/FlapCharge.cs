7164ca472417b18b8cd049f36fb24d2966c87e9a
zoe-k <5705621+zoe-k@users.noreply.github.com>
2023-05-06 23:17:08 +0100

    added feather flash on recharge

--- Assets/Scripts/Bird/FlapCharge.cs
+++ Assets/Scripts/Bird/FlapCharge.cs
@@ -12,10 +12,12 @@ namespace Bird
 
         private SpriteRenderer spriteRenderer;
 
-        public float flapCooldownTotalSecs = 3f;
+        [SerializeField] private Sprite rechargedSprite;
+        public float flapCooldownTotalSecs = 2.75f;
         private float rechargeIntervalSecs;
         private int cooldownIterations;
 
+        private Sprite defaultSprite;
         private Color rechargedColour;
         private Color onCooldownColour;
         private Color rechargeRGBDiffColour;
@@ -23,19 +25,19 @@ namespace Bird
         private void Start()
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
+            defaultSprite = spriteRenderer.sprite;
             onCooldownColour = new Color(0.196f, 0.196f, 0.196f);
             rechargedColour = Color.white;
             rechargeIntervalSecs = .25f;
-            flapCooldownTotalSecs = 3f;
+            flapCooldownTotalSecs = 2.75f;
         }
 
         public void UseFlap()
         {
+            isUseable = false;
             cooldownIterations = (int) Math.Ceiling(flapCooldownTotalSecs / rechargeIntervalSecs);
             var rgbDiff = (float) ((1f - onCooldownColour.r) / cooldownIterations);
             rechargeRGBDiffColour = new Color(rgbDiff, rgbDiff, rgbDiff);
-
-            isUseable = false;
             spriteRenderer.color = onCooldownColour;
             StartCoroutine(FlapCooldownRoutine(0, onCooldownColour));
         }
@@ -51,6 +53,9 @@ namespace Bird
                 StartCoroutine(FlapCooldownRoutine(currI + 1, newColour));
             } else {
                 spriteRenderer.color = rechargedColour;
+                spriteRenderer.sprite = rechargedSprite;
+                yield return new WaitForSeconds(rechargeIntervalSecs);
+                spriteRenderer.sprite = defaultSprite;
                 isUseable = true;
             }
         }
