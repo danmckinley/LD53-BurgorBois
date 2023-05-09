3773a27e0218b907917f7989d7bba33617cedb5d
PorgiesPie <melonmuncher1007@gmail.com>
2023-05-09 19:40:57 +0100

    did some stuff

--- Assets/Scripts/Objectives/LandingPadController.cs
+++ Assets/Scripts/Objectives/LandingPadController.cs
@@ -1,14 +1,8 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class LandingPadController : MonoBehaviour
 {
-    private CapsuleCollider2D capsuleCollider;
-
-    private void Start()
-    {
-        capsuleCollider = GetComponent<CapsuleCollider2D>();
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag.Equals("Baby"))
